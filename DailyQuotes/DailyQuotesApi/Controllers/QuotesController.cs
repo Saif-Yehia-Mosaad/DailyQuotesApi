06ca3fbdb2e6a7cd11bc85317d357938a3725c24
Saif-Abo_Seada <saifyehia58@gmail.com>
2025-12-25 15:26:39 +0200

    Add files via upload

---
+++ DailyQuotes/DailyQuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DailyQuotesApi.Models;
+
+namespace DailyQuotesApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuotesController : ControllerBase
+    {
+        private static readonly List<Quote> Quotes = new()
+        {
+            new Quote { Id = 1, Text = "Talk is cheap. Show me the code.", Author = "Linus Torvalds" },
+            new Quote { Id = 2, Text = "First, solve the problem. Then, write the code.", Author = "John Johnson" }
+        };
+
+        [HttpGet("random")]
+        public IActionResult GetRandom()
+        {
+            var quote = Quotes[new Random().Next(Quotes.Count)];
+            return Ok(quote);
+        }
+    }
+}
---
+++ DailyQuotes/DailyQuotesApi/Models/Quote.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyQuotesApi.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = "";
+        public string Author { get; set; } = "";
+    }
+}
